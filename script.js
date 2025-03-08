337639f053695ed8e860390539d7569b56a39469
Davide Gamba <gamba.davide1996@gmail.com>
2025-03-08 17:30:21 +0100

    Implementing the computer choice

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
 
-console.log("Ciao Mondo")
+
+function getComputerChoice() {
+
+  //Return 0 , 1 or 2
+  numChoice = Math.floor(Math.random() * 3)
+
+  if (numChoice === 0) {
+    return "Rock"
+  } else if(numChoice === 1) {
+    return "Paper"
+  } else {
+    return "Scissors"
+  }
+  
+}
+
+test = getComputerChoice()
+console.log(test)
